cd07a56f925df196547e5768ee0df10383cf4a77
Owain R Hunt <owainhunt@btinternet.com>
2010-04-03 23:33:20 +0100

    Implemented repository methods. Added stubs for labels, issues, comments and users methods.

--- UAGithubEngine.h
+++ UAGithubEngine.h
@@ -23,5 +23,52 @@
 
 - (id)initWithUsername:(NSString *)aUsername apiKey:(NSString *)aKey delegate:(id)theDelegate;
 - (id)sendRequest:(NSString *)path withParameters:(NSDictionary *)params;
+
+#pragma mark Repositories
+
 - (id)getRepositoriesForUser:(NSString *)aUser withWatched:(BOOL)watched;
+- (id)getRepository:(NSString *)repositoryPath;
+
+#pragma mark Issues 
+
+/*
+ 
+ - (id)getIssuesForRepository:(NSString *)repositoryPath;
+ - (id)getIssue:(NSString *)issuePath;
+ - (id)editIssue:(NSString *)issuePath withDictionary:(NSDictionary *)issueDictionary;
+ - (id)addIssueForRepository:(NSString *)repositoryPath withDictionary:(NSDictionary *)issueDictionary;
+ - (id)closeIssue:(NSString *)issuePath;
+ - (id)reopenIssue:(NSString *)issuePath;
+
+*/
+
+#pragma mark Labels
+
+/*
+ 
+ - (id)getLabelsForRepository:(NSString *)repositoryPath;
+ - (id)getIssuesForLabel:(NSString *)label;
+ - (id)addLabelForRepository:(NSString *)repositoryPath;
+ - (id)addLabel:(NSString *)label toIssue:(NSString *)issuePath;
+ - (id)removeLabel:(NSString *)label fromIssue:(NSString *)issuePath;
+ 
+*/
+
+#pragma mark Comments
+
+/*
+ 
+ - (id)getCommentsForIssue:(NSString *)issuePath;
+ - (id)addComment:(NSString *)comment toIssue:(NSString *)issuePath;
+
+*/
+ 
+#pragma mark Users
+
+/*
+ 
+ - (id)getUser:(NSString *)username;
+ 
+*/
+
 @end
