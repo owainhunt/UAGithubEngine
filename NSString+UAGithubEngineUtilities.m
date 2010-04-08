09a8c072885a322a709b94ad7106e4efde5a5a43
Owain R Hunt <owainhunt@btinternet.com>
2010-04-08 18:19:50 +0100

    Added NSString category, creates NSDates from github's date strings. Added dateElements iVar to XMLParser.

---
+++ NSString+UAGithubEngineUtilities.m
@@ -0,0 +1,18 @@
+//
+//  NSString+UAGithubEngineUtilities.m
+//  UAGithubEngine
+//
+//  Created by Owain Hunt on 08/04/2010.
+//  Copyright 2010 Owain R Hunt. All rights reserved.
+//
+
+#import "NSString+UAGithubEngineUtilities.h"
+
+
+@implementation NSString(UAGithubEngineUtilities)
+
+- (NSDate *)dateFromGithubDateString {
+	return [NSDate dateWithString:[NSString stringWithFormat:@"%@ %@ %@%@%@", [self substringToIndex:10], [self substringWithRange:NSMakeRange(11, 8)], [self substringWithRange:NSMakeRange(19, 1)], [self substringWithRange:NSMakeRange(20, 2)], [self substringFromIndex:23]]];
+}
+
+@end
