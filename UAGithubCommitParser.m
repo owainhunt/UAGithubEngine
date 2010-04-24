732ef72a003abe0fe9e09ff175a226dc2c896cdf
Owain R Hunt <owainhunt@btinternet.com>
2010-04-24 22:55:51 +0100

    Added empty files for Commit parser classes.

---
+++ UAGithubCommitParser.m
@@ -0,0 +1,14 @@
+//
+//  UAGithubCommitParser.m
+//  UAGithubEngine
+//
+//  Created by Owain Hunt on 24/04/2010.
+//  Copyright 2010 Owain R Hunt. All rights reserved.
+//
+
+#import "UAGithubCommitParser.h"
+
+
+@implementation UAGithubCommitParser
+
+@end
---
+++ UAGithubFileCommitsParser.h
@@ -0,0 +1,17 @@
+//
+//  UAGithubFileCommitsParser.h
+//  UAGithubEngine
+//
+//  Created by Owain Hunt on 24/04/2010.
+//  Copyright 2010 Owain R Hunt. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "UAGithubXMLParser.h"
+
+
+@interface UAGithubFileCommitsParser : UAGithubXMLParser {
+
+}
+
+@end
