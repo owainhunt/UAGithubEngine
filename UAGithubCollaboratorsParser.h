ecca626bf10019edad3962302a5f5b972bf6f662
Owain R Hunt <owainhunt@btinternet.com>
2010-04-29 18:56:35 +0100

    Added CollaboratorsParser. Added relevant delegate methods for new repository methods.

---
+++ UAGithubCollaboratorsParser.h
@@ -0,0 +1,17 @@
+//
+//  UAGithubCollaboratorsParser.h
+//  UAGithubEngine
+//
+//  Created by Owain Hunt on 29/04/2010.
+//  Copyright 2010 Owain R Hunt. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "UAGithubXMLParser.h"
+
+
+@interface UAGithubCollaboratorsParser : UAGithubXMLParser {
+
+}
+
+@end
