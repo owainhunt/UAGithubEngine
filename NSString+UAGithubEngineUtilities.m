8d1bb49a4438f5c573e12b0e402c80e084f1b966
Owain R Hunt <owainhunt@btinternet.com>
2010-08-01 17:59:05 +0100

    Added date parsing for third (count 'em) date format in API. Enhancement request filed: http://support.github.com/discussions/api/59-date-formats-differ-across-api

--- NSString+UAGithubEngineUtilities.m
+++ NSString+UAGithubEngineUtilities.m
@@ -13,13 +13,23 @@
 
 - (NSDate *)dateFromGithubDateString {
 	
-	// Because Github returns two different date string formats throughout the API, 
+	// Because Github returns three different date string formats throughout the API, 
 	// we need to check how to process the string based on the format used
-	if ([[self substringWithRange:NSMakeRange(10, 1)] isEqualToString:@"T"]) 
+	if ([[self characterAtIndex:10] isEqualToString:@"T"])
 	{
-		return [NSDate dateWithString:[NSString stringWithFormat:@"%@ %@ %@%@%@", [self substringToIndex:10], [self substringWithRange:NSMakeRange(11, 8)], [self substringWithRange:NSMakeRange(19, 1)], [self substringWithRange:NSMakeRange(20, 2)], [self substringFromIndex:23]]];
+		if ([[self characterAtIndex:[self length] - 1] isEqualToString:@"Z"])
+			// eg 2010-05-23T21:26:03.921Z (UTC with milliseconds)
+		{
+			return [NSDate dateWithString:[NSString stringWithFormat:@"%@ %@ +0000", [self substringToIndex:10], [self substringWithRange:NSMakeRange(11, 8)]]];
+		}
+		else 
+			// eg 2010-04-07T12:50:15-07:00
+		{
+			return [NSDate dateWithString:[NSString stringWithFormat:@"%@ %@ %@%@", [self substringToIndex:10], [self substringWithRange:NSMakeRange(11, 8)], [self substringWithRange:NSMakeRange(19, 3)], [self substringFromIndex:23]]];
+		}
 	}	
 		
+	// eg 2010/07/28 21:21:00 +0100
 	return [NSDate dateWithString:[self stringByReplacingOccurrencesOfString:@"/" withString:@"-"]];
 	
 }
