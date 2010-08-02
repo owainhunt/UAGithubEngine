bb3489d6b5c789f6b7758fff57df18b97ba067df
Owain R Hunt <owainhunt@btinternet.com>
2010-08-02 21:06:23 +0100

    Moved all parsing to SimpleJSONParser that can be moved.

--- UAGithubEngineDelegate.h
+++ UAGithubEngineDelegate.h
@@ -17,18 +17,33 @@
 
 @optional
 
+#pragma mark Users
+- (void)usersReceived:(NSArray *)users forConnection:(NSString *)connectionIdentifier;
+
+#pragma mark Repositories
 - (void)repositoriesReceived:(NSArray *)repositories forConnection:(NSString *)connectionIdentifier;
 - (void)deployKeysReceived:(NSArray *)deployKeys forConnection:(NSString *)connectionIdentifier;
 - (void)collaboratorsReceived:(NSArray *)collaborators forConnection:(NSString *)connectionIdentifier;
 - (void)languagesReceived:(NSArray *)languages forConnection:(NSString *)connectionIdentifier;
 - (void)tagsReceived:(NSArray *)tags forConnection:(NSString *)connectionIdentifier;
 - (void)branchesReceived:(NSArray *)branches forConnection:(NSString *)connectionIdentifier;
-- (void)issuesReceived:(NSArray *)issues forConnection:(NSString *)connectionIdentifier;
-- (void)issueCommentsReceived:(NSArray *)issueComments forConnection:(NSString *)connectionIdentifier;
-- (void)labelsReceived:(NSArray *)labels forConnection:(NSString *)connectionIdentifier;
-- (void)usersReceived:(NSArray *)users forConnection:(NSString *)connectionIdentifier;
+
+#pragma mark Commits
 - (void)commitsReceived:(NSArray *)commits forConnection:(NSString *)connectionIdentifier;
+
+#pragma mark Issues
+- (void)issuesReceived:(NSArray *)issues forConnection:(NSString *)connectionIdentifier;
+
+#pragma mark Labels
+- (void)labelsReceived:(NSArray *)labels forConnection:(NSString *)connectionIdentifier;
+
+#pragma mark Comments
+- (void)issueCommentsReceived:(NSArray *)issueComments forConnection:(NSString *)connectionIdentifier;
+
+#pragma mark Trees
 - (void)treeReceived:(NSArray *)treeContents forConnection:(NSString *)connectionIdentifier;
+
+#pragma mark Blobs
 - (void)blobsReceieved:(NSArray *)blobs forConnection:(NSString *)connectionIdentifier;
 - (void)blobReceived:(NSArray *)blob forConnection:(NSString *)connectionIdentifier;
 - (void)rawBlobReceived:(NSData *)blob forConnection:(NSString *)connectionIdentifier;
