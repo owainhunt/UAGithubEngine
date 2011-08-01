b3a0ed0e12f6dd24bb4e58ac8f1f5f1350cff7a1
Jeff Kelley <SlaunchaMan@gmail.com>
2011-08-01 11:58:37 -0400

    Memory management fixes.

--- NSString+UAGithubEngineUtilities.m
+++ NSString+UAGithubEngineUtilities.m
@@ -37,7 +37,7 @@
 			[newDate deleteCharactersInRange:NSMakeRange(22, 1)];
 			[df setDateFormat:@"yyyy-MM-dd'T'HH:mm:ssZZZ"];
 			[dateString release];
-			dateString = newDate;
+			dateString = [newDate retain];
 			[newDate release];
 		}
 	} else {
@@ -45,15 +45,20 @@
 		[df setDateFormat:@"yyyy/MM/dd HH:mm:ss ZZZ"];
 	}
 	
-    return [df dateFromString:dateString];
+    return [df dateFromString:[dateString autorelease]];
 
 }
 
 
 - (NSString *)encodedString
 {
-    return (NSString *)CFURLCreateStringByAddingPercentEscapes(NULL, (CFStringRef)self, NULL, (CFStringRef)@";/?:@&=$+{}<>,", kCFStringEncodingUTF8);
-
+	CFStringRef string = CFURLCreateStringByAddingPercentEscapes(NULL,
+																 (CFStringRef)self,
+																 NULL,
+																 (CFStringRef)@";/?:@&=$+{}<>,",
+																 kCFStringEncodingUTF8);
+	
+	return [(NSString *)string autorelease];
 }
 
 
