77d5421424b01a76b8be1cd224ffbc1ead0050b4
Owain R Hunt <owain@underscoreapps.com>
2011-09-15 18:01:14 +0100

    Add milestone method calls to sample AppController.

--- AppController.m
+++ AppController.m
@@ -12,57 +12,63 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification 
 {
-	githubEngine = [[UAGithubEngine alloc] initWithUsername:@"aUser" password:@"aPassword" delegate:self withReachability:YES];
+	githubEngine = [[UAGithubEngine alloc] initWithUsername:@"owainhunt" password:@"23twyx24" delegate:self withReachability:YES];
 
-	[githubEngine user:githubEngine.username];
-	//[githubEngine searchUsers:@"owainhunt" byEmail:NO];
-	
-	//[githubEngine repositoriesForUser:@"owainhunt" includeWatched:YES];
-	//[githubEngine repository:@"owainhunt/uagithubengine"];
-	//[githubEngine searchRepositories:@"rails"];
-	//[githubEngine updateRepository:@"owainhunt/uagithubengine" withInfo:[NSDictionary dictionaryWithObject:@"1" forKey:@"has_downloads"]];
-	//[githubEngine watchRepository:@"github/markup"];
-	//[githubEngine unwatchRepository:@"github/markup"];
-	//[githubEngine forkRepository:@"github/markup"];
-	//[githubEngine createRepositoryWithInfo:[NSDictionary dictionaryWithObject:@"APICreation" forKey:@"name"]];
-	//[githubEngine publiciseRepository:@"uagithubengine"];
-	//[githubEngine privatiseRepository:@"uagithubengine"];
-	//[githubEngine deployKeysForRepository:@"uagithubengine"];
-	//[githubEngine addDeployKey:@"" withTitle:@"Test Key" ToRepository:@"uagithubengine"];
-	//[githubEngine removeDeployKey:@"391578" fromRepository:@"uagithubengine"];
-	//[githubEngine collaboratorsForRepository:@"rails/rails"];
-	//[githubEngine addCollaborator:@"orhunt" toRepository:@"uagithubengine"];
-	//[githubEngine removeCollaborator:@"orhunt" fromRepository:@"uagithubengine"];
-	//[githubEngine pushableRepositories];
-	//[githubEngine networkForRepository:@"rails/rails"];
-	//[githubEngine languageBreakdownForRepository:@"rails/rails"];
-	//[githubEngine tagsForRepository:@"owainhunt/loops"];
-	//[githubEngine branchesForRepository:@"owainhunt/uagithubengine"];
-	
-	//[githubEngine commitsForBranch:@"owainhunt/uagithubengine/json"];
-	//[githubEngine commit:@"owainhunt/uagithubengine/e7777a70edc4f656678c6f79efcf82a44d5bd041"];
-
-	//[githubEngine issuesForRepository:@"owainhunt/uagithubengine" withRequestType:UAGithubIssuesClosedRequest];
-	//[githubEngine issue:@"owainhunt/uagithubengine/1"];
-	//[githubEngine editIssue:@"owainhunt/uagithubengine/1" withDictionary:[NSDictionary dictionaryWithObjectsAndKeys:@"Test Issue [edited]", @"title", @"Test body [edited again]", @"body", nil]];
-	//[githubEngine addIssueForRepository:@"owainhunt/UAGithubEngine" withDictionary:[NSDictionary dictionaryWithObjectsAndKeys:@"Testing basic auth", @"title", @"Test body", @"body", nil]];
-	//[githubEngine closeIssue:@"owainhunt/uagithubengine/1"];
-	//[githubEngine reopenIssue:@"owainhunt/uagithubengine/1"];
-
-	//[githubEngine labelsForRepository:@"owainhunt/uagithubengine"];
-	//[githubEngine addLabel:@"Super_Hyper_Mega_Bug" toIssue:1 inRepository:@"owainhunt/uagithubengine"];
-	//[githubEngine removeLabel:@"Mega Bug" fromIssue:1 inRepository:@"owainhunt/uagithubengine"];
-	//[githubEngine addLabel:@"Major_Bug_No_Really" toRepository:@"owainhunt/uagithubengine"];
-	//[githubEngine removeLabel:@"Feature Request" fromRepository:@"owainhunt/uagithubengine"];
-	
-	//[githubEngine getCommentsForIssue:@"owainhunt/uagithubengine/1"];
-	//[githubEngine addComment:@"This thing is still awesome." toIssue:@"owainhunt/uagithubengine/1"];
-	
-	//[githubEngine tree:@"owainhunt/uagithubengine/2af97f0e241c13345212"];
-	
-	//[githubEngine blobsForSHA:@"owainhunt/uagithubengine/f4667fc9a965b8f9438b8776ad61f0d5c5074e88"];
-	//[githubEngine blob:@"owainhunt/uagithubengine/f4667fc9a965b8f9438b8776ad61f0d5c5074e88/main.m"];
-	//[githubEngine rawBlob:@"owainhunt/uagithubengine/14d56058704dd3e046edaec20e93597867ef761e"];
+//	[githubEngine user:githubEngine.username];
+//	[githubEngine searchUsers:@"owainhunt" byEmail:NO];
+//	
+//	[githubEngine repositoriesForUser:@"owainhunt" includeWatched:YES];
+//	[githubEngine repository:@"owainhunt/uagithubengine"];
+//	[githubEngine searchRepositories:@"rails"];
+//	[githubEngine updateRepository:@"owainhunt/uagithubengine" withInfo:[NSDictionary dictionaryWithObject:@"1" forKey:@"has_downloads"]];
+//	[githubEngine watchRepository:@"github/markup"];
+//	[githubEngine unwatchRepository:@"github/markup"];
+//	[githubEngine forkRepository:@"github/markup"];
+//	[githubEngine createRepositoryWithInfo:[NSDictionary dictionaryWithObject:@"APICreation" forKey:@"name"]];
+//	[githubEngine publiciseRepository:@"uagithubengine"];
+//	[githubEngine privatiseRepository:@"uagithubengine"];
+//	[githubEngine deployKeysForRepository:@"uagithubengine"];
+//	[githubEngine addDeployKey:@"" withTitle:@"Test Key" ToRepository:@"uagithubengine"];
+//	[githubEngine removeDeployKey:@"391578" fromRepository:@"uagithubengine"];
+//	[githubEngine collaboratorsForRepository:@"rails/rails"];
+//	[githubEngine addCollaborator:@"orhunt" toRepository:@"uagithubengine"];
+//	[githubEngine removeCollaborator:@"orhunt" fromRepository:@"uagithubengine"];
+//	[githubEngine pushableRepositories];
+//	[githubEngine networkForRepository:@"rails/rails"];
+//	[githubEngine languageBreakdownForRepository:@"rails/rails"];
+//	[githubEngine tagsForRepository:@"owainhunt/loops"];
+//	[githubEngine branchesForRepository:@"owainhunt/uagithubengine"];
+//	
+//	[githubEngine commitsForBranch:@"owainhunt/uagithubengine/api3"];
+//	[githubEngine commit:@"owainhunt/uagithubengine/e7777a70edc4f656678c6f79efcf82a44d5bd041"];
+//    
+//  [githubEngine milestonesForRepository:@"owainhunt/uagithubengine"];
+//  [githubEngine createMilestoneWithInfo:[NSDictionary dictionaryWithObject:@"Test Milestone" forKey:@"title"] forRepository:@"owainhunt/uagithubengine"];
+//  [githubEngine milestone:1 forRepository:@"owainhunt/uagithubengine"];
+//  [githubEngine updateMilestone:1 forRepository:@"owainhunt/uagithubengine" withInfo:[NSDictionary dictionaryWithObject:@"Edited Test Milestone" forKey:@"title"]];
+//  [githubEngine deleteMilestone:1 forRepository:@"owainhunt/uagithubengine"];
+//
+//	[githubEngine issuesForRepository:@"owainhunt/uagithubengine" withParameters:nil requestType:UAGithubIssuesRequest];
+//	[githubEngine issue:1 inRepository:@"owainhunt/uagithubengine"];
+//	[githubEngine editIssue:@"owainhunt/uagithubengine/1" withDictionary:[NSDictionary dictionaryWithObjectsAndKeys:@"Test Issue [edited]", @"title", @"Test body [edited again]", @"body", nil]];
+//	[githubEngine addIssueForRepository:@"owainhunt/UAGithubEngine" withDictionary:[NSDictionary dictionaryWithObjectsAndKeys:@"Testing basic auth", @"title", @"Test body", @"body", nil]];
+//	[githubEngine closeIssue:@"owainhunt/uagithubengine/1"];
+//	[githubEngine reopenIssue:@"owainhunt/uagithubengine/1"];
+//
+//	[githubEngine labelsForRepository:@"owainhunt/uagithubengine"];
+//	[githubEngine addLabel:@"Super_Hyper_Mega_Bug" toIssue:1 inRepository:@"owainhunt/uagithubengine"];
+//	[githubEngine removeLabel:@"Mega Bug" fromIssue:1 inRepository:@"owainhunt/uagithubengine"];
+//	[githubEngine addLabel:@"Major_Bug_No_Really" toRepository:@"owainhunt/uagithubengine"];
+//	[githubEngine removeLabel:@"Feature Request" fromRepository:@"owainhunt/uagithubengine"];
+//	
+//	[githubEngine commentsForIssue:@"owainhunt/uagithubengine/1"];
+//	[githubEngine addComment:@"This thing is still awesome." toIssue:@"owainhunt/uagithubengine/1"];
+//	
+//	[githubEngine tree:@"owainhunt/uagithubengine/2af97f0e241c13345212"];
+//	
+//	[githubEngine blobsForSHA:@"owainhunt/uagithubengine/f4667fc9a965b8f9438b8776ad61f0d5c5074e88"];
+//	[githubEngine blob:@"owainhunt/uagithubengine/f4667fc9a965b8f9438b8776ad61f0d5c5074e88/main.m"];
+//	[githubEngine rawBlob:@"owainhunt/uagithubengine/14d56058704dd3e046edaec20e93597867ef761e"];
 	
 }
 
@@ -105,6 +111,12 @@
 - (void)repositoriesReceived:(NSArray *)repositories forConnection:(NSString *)connectionIdentifier
 {
 	NSLog(@"Received repositories for connection: %@, %@", connectionIdentifier, repositories);
+}
+
+
+- (void)milestonesReceived:(NSArray *)milestones forConnection:(NSString *)connectionIdentifier
+{
+    NSLog(@"Received milestones for connection: %@, %@", connectionIdentifier, milestones);
 }
 
 
