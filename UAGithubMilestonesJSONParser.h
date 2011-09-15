83c649f26b3b3aa97f16c2691efbabe2ba9f92bd
Owain R Hunt <owain@underscoreapps.com>
2011-09-15 17:53:11 +0100

    Add Milestone JSON parser.

---
+++ UAGithubMilestonesJSONParser.h
@@ -0,0 +1,14 @@
+//
+//  UAGithubMilestonesJSONParser.h
+//  UAGithubEngine
+//
+//  Created by Owain Hunt on 15/09/2011.
+//  Copyright 2011 Owain R Hunt. All rights reserved.
+//
+
+
+#import "UAGithubJSONParser.h"
+
+@interface UAGithubMilestonesJSONParser : UAGithubJSONParser
+
+@end
