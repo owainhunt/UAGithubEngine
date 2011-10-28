c8839b9f64274dbe34abdd2ade3f8da9b645a750
Owain R Hunt <owainhunt@btinternet.com>
2011-10-28 22:16:57 +0100

    @autorelease.

--- UAGithubURLConnection.m
+++ UAGithubURLConnection.m
@@ -33,18 +33,19 @@
 {
 	dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
 
-		NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-		NSURLResponse *response = nil;
-		NSError *error = nil;
-		NSData *data = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+        @autoreleasepool 
+        {    
+            NSURLResponse *response = nil;
+            NSError *error = nil;
+            NSData *data = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+            
+            if (error) {
+                failureBlock_(data,error);
+            } else {
+                successBlock_(data,response);
+            }
+        }
         
-		if (error) {
-			failureBlock_(data,error);
-		} else {
-			successBlock_(data,response);
-		}
-        
-		[pool release];
 	});
 }
 
